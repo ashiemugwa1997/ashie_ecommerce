c5d5f47d7c8a42f3e21f4bd6e2a7d59d0cb2c3fb
ashiemugwa1997 <70527457+ashiemugwa1997@gmail.noreply.github.com>
2021-11-16 00:01:35 +0200

    monday 6

--- config/app.php
+++ config/app.php
@@ -124,6 +124,7 @@ return [
         /*
          * Laravel Framework Service Providers...
          */
+        Laravel\Dusk\DuskServiceProvider::class,
         Illuminate\Auth\AuthServiceProvider::class,
         Illuminate\Broadcasting\BroadcastServiceProvider::class,
         Illuminate\Bus\BusServiceProvider::class,
